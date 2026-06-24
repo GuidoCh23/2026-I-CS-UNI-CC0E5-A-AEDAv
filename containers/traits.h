1bf2b26ac472b9d34e588dc67a575ba923040811
GuidoCh23 <guidochipana23@gmail.com>
2026-06-23 20:59:23 -0500

    refactor: adapta BTree y BTreePage a usar BTreeTrait como unico template param

--- containers/traits.h
+++ containers/traits.h
@@ -1,6 +1,7 @@
 #ifndef __TRAITS_H__
 #define __TRAITS_H__
 #include <functional> // para less y greater
+#include "../types.h"
 
 template <typename _Node, typename _Comp>
 struct BaseTrait{
@@ -14,6 +15,13 @@ struct AscendingTrait : public BaseTrait<_Node, less<typename _Node::value_type>
 };
 template <typename _Node>
 struct DescendingTrait : public BaseTrait<_Node, greater<typename _Node::value_type>>{
+};
+
+// P1 Tarea Traits
+template <typename _KeyType, typename _ObjIDType = Ref>
+struct BTreeTrait {
+    using KeyType   = _KeyType;
+    using ObjIDType = _ObjIDType;
 };
 
 #endif // __TRAITS_H__
